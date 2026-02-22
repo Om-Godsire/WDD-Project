2796191dfa278b1cfbcc47800b44ea4a411d06c1
3223-saiprasad <saiprasadchavan3223@gmail.com>
2026-02-22 12:25:43 +0530

    hasvdjasd

--- vaccum-app/src/step1/Addition.jsx
+++ vaccum-app/src/step1/Addition.jsx
@@ -6,7 +6,7 @@ let Addition= () => {
         }
     return(
         <>
-            <button className="border-1 bg-green-900 hover:bg-blue-900 shadow-md text-2xl m-2 p-2 text-white mx-auto" onClick={()=>additon_click()}>Addition</button>   
+            <button className="border-1 bg-green-900 hover:bg-blue-900 shadow-md text-2xl m-2 p-2 text-white mx-auto" onClick={()=>additon_click(num1,num2)}>Addition</button>   
         </>
     )
     }
