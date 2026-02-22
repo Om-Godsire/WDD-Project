c9ed7d01cf93654636401fb1e0bafeb71ac499a1
3223-saiprasad <saiprasadchavan3223@gmail.com>
2026-02-22 12:19:46 +0530

    edited Addition filename

--- vaccum-app/src/step1/Addition.jsx
+++ vaccum-app/src/step1/Addition.jsx
@@ -1,5 +1,15 @@
 let Addition= () => {
 
-}
+        let additon_click = () => {
+
+            alert(num1 + num2);
+        }
+    return(
+        <>
+            <button className="border-1 bg-green-900 hover:bg-blue-900 shadow-md text-2xl m-2 p-2 text-white mx-auto" onClick={()=>additon_click()}>Addition</button>   
+        </>
+    )
+    }
+
 
 export default Addition;
