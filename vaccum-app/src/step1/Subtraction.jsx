fe022db57b84052c0b82ff6fddfd74c9b78f28e5
HARSHAL KORE <harshalkore26@gmail.com>
2026-02-22 12:40:17 +0530

    - added

--- vaccum-app/src/step1/Subtraction.jsx
+++ vaccum-app/src/step1/Subtraction.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 let Subtraction=()=>{
     let subtraction_click = (num1,num2) => {
 
-            alert(num1 + num2);
+            alert(num1 - num2);
         }
     return(
         <>
