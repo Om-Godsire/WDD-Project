9979fcc2df37cc910a8f0f23c00a126fc9710e69
Om <godsire.om@example.com>
2026-02-22 12:30:01 +0530

    add

--- vaccum-app/src/step1/Addition.jsx
+++ vaccum-app/src/step1/Addition.jsx
@@ -1,12 +1,13 @@
 let Addition= () => {
 
-        let additon_click = (num1,num2) => {
-
+        let additon_click = () => {
+            let num1 = parseFloat(document.getElementById("num1").value);
+            let num2 = parseFloat(document.getElementById("num2").value);
             alert(num1 + num2);
         }
     return(
         <>
-            <button className="border-1 bg-green-900 hover:bg-blue-900 shadow-md text-2xl m-2 p-2 text-white mx-auto" onClick={()=>additon_click(num1,num2)}>Addition</button>   
+            <button className="border-1 bg-green-900 hover:bg-blue-900 shadow-md text-2xl m-2 p-2 text-white mx-auto" onClick={()=>additon_click()}>Addition</button>   
         </>
     )
     }
