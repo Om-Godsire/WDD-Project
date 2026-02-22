6e1dc7bdb6eea6c78a65ff4e50b8289ab9a1b5ca
HARSHAL KORE <harshalkore26@gmail.com>
2026-02-22 12:45:43 +0530

    updated

--- vaccum-app/src/step1/Subtraction.jsx
+++ vaccum-app/src/step1/Subtraction.jsx
@@ -2,8 +2,9 @@ import Button from "./Button";
 
 
 let Subtraction=()=>{
-    let subtraction_click = (num1,num2) => {
-
+    let subtraction_click = () => {
+            let num1 = parseFloat(document.getElementById("num1").value);
+            let num2 = parseFloat(document.getElementById("num2").value);
             alert(num1 - num2);
         }
     return(
