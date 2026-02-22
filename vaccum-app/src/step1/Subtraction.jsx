a9eb77cd19fad153a36a4fc32839b4d90fd42489
HARSHAL KORE <harshalkore26@gmail.com>
2026-02-22 12:33:32 +0530

    subtraction button added

--- vaccum-app/src/step1/Subtraction.jsx
+++ vaccum-app/src/step1/Subtraction.jsx
@@ -1,9 +1,14 @@
 let Subtraction=()=>{
+    let subtraction_click = (num1,num2) => {
+
+            alert(num1 + num2);
+        }
     return(
         <>
-        
+            <button className="border-1 bg-green-900 hover:bg-blue-900 shadow-md text-2xl m-2 p-2 text-white mx-auto" onClick={()=>subtraction_click()}>Subtraction</button>   
         </>
     )
-}
+    }
+
 
 export default Subtraction;
