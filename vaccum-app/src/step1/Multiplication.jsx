19271e0c085235734027a7230578ec676c5f8d52
rujal122a <rujal.shinde@walchandsangli.ac.in>
2026-02-22 12:32:01 +0530

    Added Multiplication Logic

--- vaccum-app/src/step1/Multiplication.jsx
+++ vaccum-app/src/step1/Multiplication.jsx
@@ -1,5 +1,17 @@
 let Multiplication = () => {
 
+    
+        let additon_click = () => {
+            let num1 = parseFloat(document.getElementById("num1").value);
+            let num2 = parseFloat(document.getElementById("num2").value);
+            alert(num1 * num2);
+        }
+    return(
+        <>
+            <button className="border-1 bg-green-900 hover:bg-blue-900 shadow-md text-2xl m-2 p-2 text-white mx-auto" onClick={()=>additon_click()}>Addition</button>   
+        </>
+    )
+
 }
 
 export default Multiplication;
